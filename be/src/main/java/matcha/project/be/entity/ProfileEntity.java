8c46c1efc980f983ab83f081737e44aab8df2d17
Duyvu2610 <duyvu2612003@gmail.com>
2024-10-29 23:00:25 +0700

    add source be

---
+++ be/src/main/java/matcha/project/be/entity/ProfileEntity.java
@@ -0,0 +1,45 @@
+package matcha.project.be.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import matcha.project.be.common.entity.SystemField;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "t_profile")
+public class ProfileEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "country_id")
+    private Integer countryId;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "birth_date")
+    private LocalDateTime birthDate;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @Column(name = "address_line")
+    private String addressLine;
+
+    @Embedded
+    private SystemField systemField;
+}
