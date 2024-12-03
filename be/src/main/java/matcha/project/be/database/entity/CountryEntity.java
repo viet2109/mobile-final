ebf13ebb1b0424b2699e6d64091bf0862dfa2d91
viet21092003 <21124548@st.hcmuaf.edu.vn>
2024-12-03 16:52:21 +0700

    rechange be src

---
+++ be/src/main/java/matcha/project/be/database/entity/CountryEntity.java
@@ -0,0 +1,32 @@
+package matcha.project.be.database.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import matcha.project.be.common.entity.SystemField;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Data
+@Table(name = "t_country")
+public class CountryEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "telephone_code")
+    private String telephoneCode;
+
+    @Embedded
+    private SystemField systemField;
+}
