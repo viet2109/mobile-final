298b95fdca56f2064d0dbbc4c47c149c99db9ef1
viet21092003 <21124548@st.hcmuaf.edu.vn>
2024-12-19 22:07:03 +0700

    add db

--- fe/app/[...unmatched].tsx
+++ fe/app/[...unmatched].tsx
@@ -1,7 +1,9 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 
-import { Stack, useRouter } from "expo-router";
-const Welcome = () => {
+import { useRouter } from "expo-router";
+import { Stack, Link } from "expo-router";
+const PageNotFound = () => {
   const router = useRouter();
 
   return (
@@ -18,32 +20,32 @@ const Welcome = () => {
         <View className=" justify-center items-center  ">
           <Image
             className="ml-8"
-            source={require("../assets/images/Welcome.jpg")}
+            source={require("../assets/images/404Page.jpg")}
             style={{
               width: "85%",
               height: "60%",
             }}
             resizeMode="contain"
           />
-          <Text className=" px-10 mb-2  text-4xl font-bold  text-center">
-            Congratulations!{" "}
+          <Text className=" px-10 mb-2  text-3xl font-bold  text-center">
+            Error 404{" "}
           </Text>
-          <Text className=" px-6 mb-4  text-4xl font-bold  text-center">
-            Welcome to Coinpay
+          <Text className=" px-10 mb-2  text-3xl font-bold  text-center">
+            Page Not Found
           </Text>
           <Text className=" text-sm px-6 text-gray-500 text-center">
-            We are happy to have you. It's time to send, receive and track your
-            expense.{" "}
+            OOps! It looks like the page you're looking for doesn't exist or has
+            been moved. Please try again or go back to the home page.{" "}
           </Text>
         </View>
 
-        <View className=" w-full items-center mt-10">
+        <View className=" w-full items-center">
           <TouchableOpacity
             className="bg-blue-bg py-3 px-6 rounded-full w-4/5"
             onPress={() => router.push("home")}
           >
             <Text className="text-white text-lg font-bold text-center ">
-              Continue
+              Back to Home
             </Text>
           </TouchableOpacity>
         </View>
@@ -52,4 +54,4 @@ const Welcome = () => {
   );
 };
 
-export default Welcome;
+export default PageNotFound;
