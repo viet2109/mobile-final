fb1bb4aeda71a751ec3dcf29f54afc16e38e12cd
Mai Xuân Thức <xuanthuc254@gmail.com>
2025-01-01 15:09:01 +0700

    fix crash and mofify fe

--- fe/components/Spending/chart.tsx
+++ fe/components/Spending/chart.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { LineChart } from "react-native-chart-kit";
-import { Dimensions } from "react-native";
+import { Dimensions, Text, View, StyleSheet } from "react-native";
 
 interface TransactionItem {
   id: string;
@@ -15,39 +15,19 @@ interface ChartProps {
 const Chart: React.FC<ChartProps> = ({ transactions }) => {
   const screenWidth = Dimensions.get("window").width;
 
-  //  transactions rong~
+  // Kiểm tra nếu transactions rỗng
   if (!transactions || transactions.length === 0) {
-    // 
-    const emptyChartData = {
-      labels: [], 
-      datasets: [{ data: [] }], 
-    };
-
     return (
-      <LineChart
-        data={emptyChartData}
-        width={screenWidth - 30}
-        height={200}
-        chartConfig={{
-          backgroundColor: "#ffffff",
-          backgroundGradientFrom: "#ffffff",
-          backgroundGradientTo: "#ffffff",
-          decimalPlaces: 0,
-          color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-        }}
-        style={{
-          borderRadius: 10,
-          marginBottom: 20,
-        }}
-      />
+      <View style={styles.container}>
+        <Text style={styles.text}>Không có dữ liệu giao dịch!!!</Text>
+      </View>
     );
   }
 
-  // tao label va data tu transaction
+  // Tạo label và data từ transactions
   const labels = transactions.map((transaction) => {
     const [day, month, year] = transaction.transactionDate.split(" ");
-    return `${day}-${year}`; 
+    return `${day}-${year}`;
   });
 
   const data = transactions.map((transaction) => transaction.amount);
@@ -77,5 +57,18 @@ const Chart: React.FC<ChartProps> = ({ transactions }) => {
     />
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10
+  },
+  text: {
+    fontSize: 16,
+    color: "gray",
+  },
+});
 
 export default Chart;
